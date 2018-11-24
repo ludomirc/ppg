eb9603a555482b2de7d5c2e6a654b34568b4d12a
benek <beniamin.czaplicki@gmail.com>
2018-11-24 06:49:30 +0100

    after buld

--- buffor1/Scene.cpp
+++ buffor1/Scene.cpp
@@ -1,7 +1,7 @@
 #include "Scene.h"
 #include "Utilities.h"
 
-Scene::Scene(void) : vbo_handle(0u), vao_handle(0u), simple_program(0u)
+Scene::Scene(void) : vbo_handle(0u), index_buffer_handle(0u), vao_handle(0u)
 {
 }
 
@@ -40,6 +40,9 @@ bool Scene::init(void)
 		0.f, 0.5f, 0.f
 	};
 
+    // stworzenie tablicy z danymi o indeksach
+    gl::GLushort indices[] = { 0, 1, 2 };
+
     std::cout << "Generating buffers..." << std::endl;
     // stworzenie bufora
     gl::glGenBuffers(1, &vbo_handle);
@@ -49,6 +52,15 @@ bool Scene::init(void)
     gl::glBufferData(gl::GL_ARRAY_BUFFER, 9 * sizeof(gl::GLfloat), vertices, gl::GL_STATIC_DRAW);
     // odbindowanie buffora zbindowanego jako VBO (zeby przypadkiem nie narobic sobie klopotow...)
     gl::glBindBuffer(gl::GL_ARRAY_BUFFER, 0);
+
+    // stworzenie bufora
+    gl::glGenBuffers(1, &index_buffer_handle);
+    // zbindowanie bufora jako IB
+    gl::glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle);
+    // alokacja pamieci dla bufora zbindowanego jako IB i skopiowanie danych z tablicy "indeices"
+    gl::glBufferData(gl::GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(gl::GLushort), indices, gl::GL_STATIC_DRAW);
+    // odbindowanie buffora zbindowanego jako IB (zeby przypadkiem nie narobic sobie klopotow...)
+    gl::glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // stworzenie VAO
     gl::glGenVertexArrays(1, &vao_handle);
@@ -61,15 +73,19 @@ bool Scene::init(void)
     gl::glVertexAttribPointer(vertex_position_loction, 3, gl::GL_FLOAT, gl::GL_FALSE, sizeof(float)* 3, (const gl::GLvoid*)0);
     // odblokowanie mozliwosci wczytywania danych z danej lokalizacji
     gl::glEnableVertexAttribArray(vertex_position_loction);
-    // odbindowanie VAO (ma ono teraz informacje m.in. o VBO, wiec gdy zajdzie potrzeba uzycia VBO, wystarczy zbindowac VAO)
+    // zbindowanie IB do aktualnego VAO
+    gl::glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle);
+    // odbindowanie VAO (ma ono teraz informacje m.in. o VBO + IB, wiec gdy zajdzie potrzeba uzycia VBO + IB, wystarczy zbindowac VAO)
     gl::glBindVertexArray(0u);
 
     // odbindowanie buffora zbindowanego jako VBO (zeby przypadkiem nie narobic sobie klopotow...)
     gl::glBindBuffer(gl::GL_ARRAY_BUFFER, 0);
+    // odbindowanie buffora zbindowanego jako bufor indeksow (zeby przypadkiem nie narobic sobie klopotow...)
+    gl::glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // na tym mozna zakonczyc
     // w tej prostej aplikacji bedziemy rysowac tylko 1 (powyzej stworzony) model, dlatego mozemy juz teraz ustawic odpowiednie dane dla naszych shaderow (nie beda sie one zmieniac co klatke...)
-    // zeby narysowac nasz model musimy ustawic odpowiednie VBO (a dzieki temu ze VAO ma o nim iformacje sprowadza sie to do ustawienia odpowiedniego VAO)
+    // zeby narysowac nasz model musimy ustawic odpowiednie VBO + IB (a dzieki temu ze VAO ma o nich iformacje sprowadza sie to do ustawienia odpowiedniego VAO, a przez to reszte buforow)
     // Czyli znowu bindujemy VAO
     gl::glBindVertexArray(vao_handle);
 
@@ -79,7 +95,7 @@ bool Scene::init(void)
 bool Scene::draw(float delta_time)
 {
     // rozpoczynamy rysowanie uzywajac ustawionego programu (shaderow) i ustawionych buforow
-    gl::glDrawArrays(gl::GL_TRIANGLES, 0, 9);
+    gl::glDrawElements(gl::GL_TRIANGLES, 3, gl::GL_UNSIGNED_SHORT, 0);
 
 	return true;
 }
@@ -102,6 +118,15 @@ void Scene::release(void)
         // usuniecie VBO
         gl::glDeleteBuffers(1, &vbo_handle);
         vbo_handle = 0;
+    }
+
+    // odbindowanie IB
+    glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, 0);
+    if (index_buffer_handle)
+    {
+        // usuniecie IB
+        gl::glDeleteBuffers(1, &index_buffer_handle);
+        index_buffer_handle = 0;
     }
 
     // ustawienie aktywnego programu na 0 (zaden)
