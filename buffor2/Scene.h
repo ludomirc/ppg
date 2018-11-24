be9118361aafc4833572f3960263c112e3f7edbd
benek <beniamin.czaplicki@gmail.com>
2018-11-24 13:38:17 +0100

    added buffor2 oriject

---
+++ buffor2/Scene.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "libs.h"
+
+class Scene
+{
+    gl::GLuint vbo_handle;
+    gl::GLuint index_buffer_handle;
+    gl::GLuint vao_handle;
+    gl::GLuint simple_program;
+
+public:
+	Scene(void);
+	~Scene(void);
+
+    // metoda wywolywana na poczatku (przy starcie aplikacji, zaraz po inicjalizacji OGL)
+    bool init(void);
+    // metoda wywolywana co klatke
+	bool draw(float delta_time);
+    // metoda wywolywana przy zamknieciu aplikacji
+	void release(void);
+};
