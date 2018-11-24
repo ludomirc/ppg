52972113a68318cba73878018e7848826026518d
benek <beniamin.czaplicki@gmail.com>
2018-11-24 13:09:48 +0100

    fix buffer issue

--- buffor1/Scene.cpp
+++ buffor1/Scene.cpp
@@ -35,13 +35,14 @@ bool Scene::init(void)
 
     // stworzenie tablicy z danymi o wierzcholkach 3x (x, y, z)
     gl::GLfloat vertices[] = {
-		-0.5f, 0.f, 0.f, 0.583f,  0.771f,  0.014f,
-		0.5f, 0.f, 0.f, 0.609f,  0.115f,  0.436f,
-		0.f, 0.5f, 0.f, 0.327f,  0.483f,  0.844f
-	};
+            -0.5,-0.5,0.0, 1.0, 0.0, 0.0,
+            0.5,-0.5,0.0, 0.0, 1.0, 0.0,
+            -0.5,0.5,0.0, 0.0, 0.0, 1.0
+
+    };
 
     // stworzenie tablicy z danymi o indeksach
-    gl::GLushort indices[] = { 0, 1, 2 };
+    gl::GLushort indices[] = {0,1,2};
 
     std::cout << "Generating buffers..." << std::endl;
     // stworzenie bufora
@@ -49,7 +50,7 @@ bool Scene::init(void)
     // zbindowanie bufora jako VBO
     gl::glBindBuffer(gl::GL_ARRAY_BUFFER, vbo_handle);
     // alokacja pamieci dla bufora zbindowanego jako VBO i skopiowanie danych z tablicy "vertices"
-    gl::glBufferData(gl::GL_ARRAY_BUFFER, 9 * sizeof(gl::GLfloat), vertices, gl::GL_STATIC_DRAW);
+    gl::glBufferData(gl::GL_ARRAY_BUFFER, 3 * 6 * sizeof(gl::GLfloat), vertices, gl::GL_STATIC_DRAW);
     // odbindowanie buffora zbindowanego jako VBO (zeby przypadkiem nie narobic sobie klopotow...)
     gl::glBindBuffer(gl::GL_ARRAY_BUFFER, 0);
 
@@ -70,23 +71,20 @@ bool Scene::init(void)
     // zbindowanie VBO do aktualnego VAO
     gl::glBindBuffer(gl::GL_ARRAY_BUFFER, vbo_handle);
     // ustalenie jak maja byc interpretowane dane z VBO
-    gl::glVertexAttribPointer(vertex_position_loction, 3, gl::GL_FLOAT, gl::GL_FALSE, sizeof(float)* 3, (const gl::GLvoid*)0);
-    // odblokowanie mozliwosci wczytywania danych z danej lokalizacji
-    gl::glEnableVertexAttribArray(vertex_position_loction);
+    gl::glVertexAttribPointer(0u, 3 , gl::GL_FLOAT, gl::GL_FALSE,6*sizeof(float),(const gl::GLvoid*)(0) );
+    gl::glEnableVertexAttribArray(0u);
+
+    gl::GLuint vcolor_position_loction {1u};
+    gl::glVertexAttribPointer(vcolor_position_loction, 3, gl::GL_FLOAT, gl::GL_FALSE, sizeof(float)* 6, (const gl::GLvoid*)(3*sizeof(float)));
+    gl::glEnableVertexAttribArray(vcolor_position_loction);
+
     // zbindowanie IB do aktualnego VAO
     gl::glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle);
+    //gl::glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, 0);
     // odbindowanie VAO (ma ono teraz informacje m.in. o VBO + IB, wiec gdy zajdzie potrzeba uzycia VBO + IB, wystarczy zbindowac VAO)
+
     gl::glBindVertexArray(0u);
 
-    // odbindowanie buffora zbindowanego jako VBO (zeby przypadkiem nie narobic sobie klopotow...)
-    gl::glBindBuffer(gl::GL_ARRAY_BUFFER, 0);
-    // odbindowanie buffora zbindowanego jako bufor indeksow (zeby przypadkiem nie narobic sobie klopotow...)
-    gl::glBindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // na tym mozna zakonczyc
-    // w tej prostej aplikacji bedziemy rysowac tylko 1 (powyzej stworzony) model, dlatego mozemy juz teraz ustawic odpowiednie dane dla naszych shaderow (nie beda sie one zmieniac co klatke...)
-    // zeby narysowac nasz model musimy ustawic odpowiednie VBO + IB (a dzieki temu ze VAO ma o nich iformacje sprowadza sie to do ustawienia odpowiedniego VAO, a przez to reszte buforow)
-    // Czyli znowu bindujemy VAO
     gl::glBindVertexArray(vao_handle);
 
 	return true;
@@ -94,6 +92,7 @@ bool Scene::init(void)
 
 bool Scene::draw(float delta_time)
 {
+    std::cerr<<"drawing \n";
     // rozpoczynamy rysowanie uzywajac ustawionego programu (shaderow) i ustawionych buforow
     gl::glDrawElements(gl::GL_TRIANGLES, 3, gl::GL_UNSIGNED_SHORT, 0);
 
